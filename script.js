e6ce8663aa5fefb791db6679d13b6d0694c8f593
Marcos Almorox <marcos.almorox@gmail.com>
2023-11-12 20:19:13 +0100

    a

--- script.js
+++ script.js
@@ -1,67 +1,72 @@
-const images = [
-    'image1.jpg',
-    'image2.jpg',
-    'image3.jpg',
-    'image4.jpg',
-    'image1.jpg',
-    'image2.jpg',
-    'image3.jpg',
-    'image4.jpg',
+const IMAGES = [
+    'imagen1.jpg',
+    'imagen2.jpg',
+    'imagen3.jpg',
+    'imagen4.jpg',
+    'imagen5.jpg',
+    'imagen6.jpg',
+    'imagen7.jpg',
+    'imagen8.jpg',
+    'imagen9.jpg',
+    'imagen10.jpg',
 ];
 
-    let flippedCards = [];
-    let pairsFound = 0;
+const IMAGES_LENGHT = images.length;
+
+let flippedCards = [];
+let pairsFound = 0;
 
 function createBoard() {
     const container = document.body;
 
-    images.forEach((image, index) => {
+    IMAGES.forEach((image, index) => {
         const card = createCard(image, index);
         container.appendChild(card);
     });
     shuffleCards();
 }
 
-    function createCard(image, index) {
-        const cardContainer = document.createElement('div');
-        cardContainer.classList.add('card');
+function createCard(image, index) {
+    const cardContainer = document.createElement('div');
+    cardContainer.classList.add('card');
 
-        const cardFront = document.createElement('div');
-        cardFront.classList.add('card-face', 'front');
-        cardFront.style.backgroundImage = `url('back.jpg')`;
+    const cardFront = document.createElement('div');
+    cardFront.classList.add('card-face');
+    cardFront.style.backgroundImage = `url('back.jpg')`;
+    cardFront.addEventListener('click', flipCard);
 
-        const cardBack = document.createElement('div');
-        cardBack.classList.add('card-face', 'back');
-        cardBack.style.backgroundImage = `url('${image}')`;
+    const cardBack = document.createElement('div');
+    cardBack.classList.add('card-face');
+    cardBack.style.backgroundImage = `url(img/'${image}')`;
 
-        cardContainer.appendChild(cardFront);
-        cardContainer.appendChild(cardBack);
+    cardContainer.appendChild(cardFront);
+    cardContainer.appendChild(cardBack);
 
-        cardContainer.addEventListener('click', function () {
-            if (!this.classList.contains('flipped') && flippedCards.length < 2) {
-                flipCard(this);
-                flippedCards.push(this);
+    cardContainer.addEventListener('click', function () {
+        if (!this.classList.contains('flipped') && flippedCards.length < 2) {
+            flipCard(this);
+            flippedCards.push(this);
 
-                if (flippedCards.length === 2) {
-                    setTimeout(checkForMatch, 1000);
-                }
+            if (flippedCards.length === 2) {
+                setTimeout(checkForMatch, 1000);
             }
-        });
+        }
+    });
+    return cardContainer;
+}
 
-        return cardContainer;
-    }
 
-    function shuffleCards() {
-        const cards = document.querySelectorAll('.card');
-        cards.forEach(card => {
-            const randomPos = Math.floor(Math.random() * images.length);
-            card.style.order = randomPos;
-        });
-    }
+function shuffleCards() {
+    const cards = document.querySelectorAll('.card');
+    cards.forEach(card => {
+        let randomPosition = Math.floor(Math.random() * IMAGES_LENGHT);
+        card.style.order = randomPosition;
+    });
+}
 
-    function flipCard(card) {
-        card.classList.add('flipped');
-    }
+function flipCard(card) {
+    card.classList.add('flipped');
+}
 
     function checkForMatch() {
         const [card1, card2] = flippedCards;
@@ -85,6 +90,5 @@ function createBoard() {
         }
 }
 
-    // Inicializar el juego al cargar la página
-    createBoard();
-});
+// Inicializar el juego al cargar la página
+createBoard();
