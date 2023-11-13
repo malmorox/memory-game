c0b9ee410ba3975db7643684eab2bf6b10cb7a97
Marcos Almorox <marcos.almorox@gmail.com>
2023-11-13 14:34:38 +0100

    Finalizando el juego

--- script.js
+++ script.js
@@ -6,45 +6,46 @@ const IMAGES = [
     'imagen13.jpg', 'imagen14.jpg', 'imagen15.jpg', 'imagen16.jpg',
 ];
 
-//variable constante para usar la cantidad de imagenes que hay
+//constante para almacenar la longitud del array de imagenes que hay
 const IMAGES_LENGHT = IMAGES.length;
-
+//array para almacenar las 2 cartas que volteamos y el recuendo de parejas encontradas para comprobar la victoria
 let flippedCards = [];
 let pairsFound = 0;
 
 function createBoard() {
-    const container = document.querySelector('.cardsContainer');
+    const CARDS_CONTAINER = document.querySelector('.cardsContainer');
     //array para duplicar las imagenes y haya 2 de cada
-    const cardImages = [...IMAGES, ...IMAGES];
-    shuffleCards(cardImages);
-    cardImages.forEach((image, index) => {
-        const card = createCard(image, index);
-        container.appendChild(card);
+    const CARD_IMAGES = [...IMAGES, ...IMAGES];
+    shuffleCards(CARD_IMAGES);
+    CARD_IMAGES.forEach((image) => {
+        let card = createCard(image);
+        CARDS_CONTAINER.appendChild(card);
     });
 }
 
 function createCard(image) {
     //contenedor general de la carta
-    const cardContainer = document.createElement('div');
+    let cardContainer = document.createElement('div');
     cardContainer.classList.add('card');
+
     //contenedor de la carta boca abajo
-    const cardFront = document.createElement('div');
-    cardFront.classList.add('card-face', 'front');
-    const cardFrontImg = document.createElement('img');
-    cardFrontImg.src = `img/carta.jpg`;
-    cardFront.addEventListener('click', flipCard);
-    //contenedor de la carta volteada
-    const cardBack = document.createElement('div');
+    let cardFront = document.createElement('div');
+    cardFront.classList.add('card-face');
+    //imagen de la carta boca abajo (que será igual para todas las cartas)
+    let cardFrontImg = document.createElement('img');
+    cardFrontImg.src = 'img/carta.jpg';
+    cardFront.appendChild(cardFrontImg);
+
+    //contenedor de la carta boca arriba (volteada)
+    let cardBack = document.createElement('div');
     cardBack.classList.add('card-face', 'back');
-    cardBack.classList.add('card-face');
-    //imagen que metemos dentro del contenedor de la carta volteada
-    const cardBackImg = document.createElement('img');
+    //imagen de la carta boca arriba (hay 16 imágenes diferentes)
+    let cardBackImg = document.createElement('img');
     cardBackImg.src = `img/${image}`;
+    cardBack.appendChild(cardBackImg);
 
     cardContainer.appendChild(cardFront);
     cardContainer.appendChild(cardBack);
-    cardFront.appendChild(cardFrontImg);
-    cardBack.appendChild(cardBackImg);
 
     cardContainer.addEventListener('click', function () {
         if (!this.classList.contains('flipped') && flippedCards.length < 2) {
@@ -52,6 +53,7 @@ function createCard(image) {
             flippedCards.push(this);
 
             if (flippedCards.length === 2) {
+                //tiempo de espera para pulsar en otra carta
                 setTimeout(checkForMatch, 1000);
             }
         }
@@ -59,20 +61,22 @@ function createCard(image) {
     return cardContainer;
 }
 
-
+//funcion para barajar las cartas
 function shuffleCards(cards) {
-    cards.sort(() => Math.random() - 0.4);
+    cards.sort(() => Math.random() - 0.5);
 }
 
+//funcion para voltear la carta
 function flipCard(card) {
     card.classList.toggle('flipped');
 }
 
+//funcion para comprobar si las 2 cartas que volteas son iguales
 function checkForMatch() {
-    const [card1, card2] = flippedCards;
-
-    const img1 = card1.querySelector('.back img').getAttribute('src');
-    const img2 = card2.querySelector('.back img').getAttribute('src');
+    let [card1, card2] = flippedCards;
+    //asignando a img1 y img2 los valores del src de las 2 imagenes que seleccionamos para comprobar si son iguales
+    let img1 = card1.querySelector('.back img').getAttribute('src');
+    let img2 = card2.querySelector('.back img').getAttribute('src');
 
     if (img1 === img2) {
         //las cartas son iguales
